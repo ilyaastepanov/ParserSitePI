9d781e11b5c8aab11b93144ed2c6a5e84296f239
Ilia Stepanov <ilyaa.stepanov@gmail.com>
2024-01-25 14:52:44 +0300

    Add Libraries and run func main

---
+++ ParserPI.py
@@ -0,0 +1,15 @@
+import os
+import pathlib
+import shutil
+import zipfile
+from glob import glob
+from pathlib import Path
+import requests
+from bs4 import BeautifulSoup
+
+
+
+
+
+if __name__ == '__main__':
+	main()
